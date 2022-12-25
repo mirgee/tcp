209ca91ce8d3bd9fd94c2b94ff18ac44c08023d0
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-12-25 23:40:18 +0100

    Boundary check on ack num
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- src/tcp.rs
+++ src/tcp.rs
@@ -4,14 +4,13 @@ use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice, Ipv4Header, ip_number::TCP};
 use tun::platform::macos::Device;
 
 pub enum State {
-    Closed,
-    Listen,
     SynRcvd,
     Established
 }
 
 /// Send Sequence Space (RFC 793, p. 20)
 /// 
+/// ```
 ///            1         2          3          4
 ///       ----------|----------|----------|----------
 ///              SND.UNA    SND.NXT    SND.UNA
@@ -21,6 +20,7 @@ pub enum State {
 /// 2 - sequence numbers of unacknowledged data
 /// 3 - sequence numbers allowed for new data transmission
 /// 4 - future sequence numbers which are not yet allowed
+/// ```
 struct SendSequence {
     /// oldest unacknowledged sequence number
     una: u32,
@@ -40,10 +40,12 @@ struct SendSequence {
 
 /// Receive Sequence Space (RFC 793, p. 20)
 /// 
+/// ```
 ///     1          2          3
 /// ----------|----------|----------
 ///        RCV.NXT    RCV.NXT
 ///                  +RCV.WND
+/// ```
 struct ReceiveSequence {
     /// next sequence number expected on an incoming segments, and
     /// is the left or lower edge of the receive window
@@ -59,7 +61,8 @@ struct ReceiveSequence {
 pub struct Connection {
     state: State,
     snd_seq: SendSequence,
-    rcv_seq: ReceiveSequence
+    rcv_seq: ReceiveSequence,
+    iph: Ipv4Header // Not great, we have to change the payload len every time
 }
 
 impl Connection {
@@ -70,17 +73,7 @@ impl Connection {
 
         let iss = 0;
         let wnd = 1024;
-        // Keep track of sender info
-        let rcv_seq = ReceiveSequence {
-            nxt: tcph.sequence_number() + 1,
-            wnd: tcph.window_size(),
-            up: 0,
-            irs: tcph.sequence_number()
-        };
-        // Just out of curiosity
-        let rcv_ackn = tcph.acknowledgment_number(); // This is relevant only if ACK is set
-        println!("Received ackn: {}", rcv_ackn);
-        
+
         // Keep track of what we're sending
         let snd_seq = SendSequence {
             una: iss,
@@ -92,11 +85,22 @@ impl Connection {
             iss
         };
 
+        // Keep track of sender info
+        let rcv_seq = ReceiveSequence {
+            nxt: tcph.sequence_number() + 1,
+            wnd: tcph.window_size(),
+            up: 0,
+            irs: tcph.sequence_number()
+        };
+        
+        // Construct TCP header
         let mut syn_ack = etherparse::TcpHeader::new(tcph.destination_port(), tcph.source_port(), 0, wnd);
         syn_ack.acknowledgment_number = rcv_seq.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
         syn_ack.checksum = syn_ack.calc_checksum_ipv4(&iph.to_header(), &[]).unwrap();
+
+        // Construct IP header
         let iph = Ipv4Header::new(syn_ack.header_len(), 64, TCP, iph.destination(), iph.source());
 
         // Construct pattern and send
@@ -112,21 +116,42 @@ impl Connection {
         Ok(Self {
             state: State::SynRcvd,
             snd_seq,
-            rcv_seq
+            rcv_seq,
+            iph
         })
     }
 
     /// RFC 793, p. 23
     pub fn on_packet(&mut self, iph: Ipv4HeaderSlice, tcph: TcpHeaderSlice, packet: &[u8]) -> std::io::Result<()> {
-    let src = iph.source_addr();
-    let dst = iph.destination_addr();
-    let sport = tcph.source_port();
-    let dport = tcph.destination_port();
-    let seq = tcph.sequence_number();
-    let ack = tcph.acknowledgment_number();
-    let win = tcph.window_size();
-    // let data = 4 + ip.slice().len() + tcph.slice().len()..4 + ip.slice().len() + len;
-    println!("{}:{} -> {}:{} seq={} ack={} win={} data_len={:?}", src, sport, dst, dport, seq, ack, win, packet.len());
-    Ok(())
+        // SND.UNA < SEG.ACK =< SND.NXT
+        let una = self.snd_seq.una;
+        let ackn = tcph.acknowledgment_number();
+        let nxt = self.snd_seq.nxt;
+        if una < ackn {
+            if una <= nxt && nxt < ackn {
+                // this is not OK?
+            } else {
+                // this is OK, either ackn <= nxt, nxt wrapped and same
+            }
+        } else {
+            if una > nxt && nxt >= ackn {
+                // this is not OK?
+            } else {
+                // this is OK
+            }
+        };
+        match self.state {
+            State::SynRcvd => {
+                if !tcph.ack() {
+                    return Err(std::io::Error::new(std::io::ErrorKind::Other, "Received non-ACK packet in syn-rcvd state"));
+                }
+                if ackn != self.snd_seq.nxt {
+                    return Err(std::io::Error::new(std::io::ErrorKind::Other, "Received ACK packet with wrong ack number"));
+                }
+                self.state = State::Established;
+            },
+            _ => {}
+        };
+        Ok(())
     }
 }
